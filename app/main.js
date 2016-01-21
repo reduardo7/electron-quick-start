058e5ac5851b1ea34d79c337bdb6d63f878469bd
eduardo_cuomo <reduardo7@gmail.com>
2016-01-20 22:17:21 -0300

    Server implement

--- app/main.js
+++ app/main.js
@@ -1,10 +1,97 @@
 'use strict';
 
 const electron = require('electron');
-// Module to control application life.
+const express = require('express');
+const appServer = express();
 const app = electron.app;
-// Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
+
+
+
+
+// Config
+var $cfg = {
+    server: {
+        port: 8087
+    }
+};
+
+// Conexión con la base de datos
+//var db = openDatabase('mydb', '1.0', 'my first database', 2 * 1024 * 1024);
+
+// Configuración
+appServer.configure(function () {
+    // Localización de los ficheros estáticos
+    appServer.use(express.static(__dirname + '/public'));
+    // Muestra un log de todos los request en la consola
+    appServer.use(express.logger('dev'));
+    // Permite cambiar el HTML con el método POST
+    appServer.use(express.bodyParser());
+    // Simula DELETE y PUT
+    appServer.use(express.methodOverride());
+});
+
+// Rutas de nuestro API
+// GET de todos los TODOs
+appServer.get('/api/todos', function(req, res) {  
+    /*Todo.find(function(err, todos) {
+        if(err) {
+            res.send(err);
+        }
+        res.json(todos);
+    });*/
+});
+
+// POST que crea un TODO y devuelve todos tras la creación
+appServer.post('/api/todos', function(req, res) {
+    /*Todo.create({
+        text: req.body.text,
+        done: false
+    }, function (err, todo){
+        if(err) {
+            res.send(err);
+        }
+
+        Todo.find (function(err, todos) {
+            if(err){
+                res.send(err);
+            }
+            res.json(todos);
+        });
+    });*/
+});
+
+// DELETE un TODO específico y devuelve todos tras borrarlo.
+appServer.delete('/api/todos/:todo', function(req, res) {
+    /*Todo.remove({
+        _id: req.params.todo
+    }, function (err, todo) {
+        if (err) {
+            res.send(err);
+        }
+
+        Todo.find(function (err, todos) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(todos);
+        });
+
+    })*/
+});
+
+// Carga una vista HTML simple donde irá nuestra Single AppServer Page
+// Angular Manejará el Frontend
+appServer.get('*', function (req, res) {
+    res.sendfile('./public/index.html');
+});
+
+// Escucha en el puerto $cfg.server.port y corre el server
+appServer.listen($cfg.server.port, function () {
+    console.log('App listening on port ' + $cfg.server.port);
+});
+
+
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -15,10 +102,10 @@ function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600});
 
   // and load the index.html of the app.
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('http://127.0.0.1:' + $cfg.server.port);
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
