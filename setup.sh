0df5b99e18b7bc33c03bcf6b2fef1f33c9901181
eduardo_cuomo <reduardo7@gmail.com>
2016-02-13 15:13:52 -0300

    sudo fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+if ! type sudo &>/dev/null
+	then
+		sudo() {
+			echo "$@"
+			"$@"
+		}
+	fi
+
 sudo npm install -g npm
 sudo npm install -g electron-packager
 sudo npm install -g electron-builder
